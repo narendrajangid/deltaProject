627bf37a573b51b8de9e3f900280f558d39b5e6f
narendrajangid <137505568+narendrajangid@users.noreply.github.com>
2024-04-30 09:41:02 -0700

    Update app.js
    aded new route

--- app.js
+++ app.js
@@ -71,9 +71,9 @@ const sessionOptions = {
 
 
 
-// app.get("/" ,(req,res) => {
-//     res.send("hi i am root");
-// })
+app.get("/" ,(req,res) => {
+    res.send("hi i am root");
+})
 
 app.use(session(sessionOptions));
 app.use(flash());
